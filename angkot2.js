931be4d7356f85f0e7ae27a9199971b6d899c976
iyas bagus <bagusiyas15@gmail.com>
2024-09-16 21:25:25 +0700

    Kembali mengulang javascript dari looping dan pengkondisian

---
+++ angkot2.js
@@ -0,0 +1,11 @@
+let jmlAngkot = 10;
+let angkotBeroperasi = 6;
+let noAngkot = 1
+
+while (noAngkot <= angkotBeroperasi) {
+    console.log('Angkot No. ' + noAngkot + ' Beroprasi dengan baik');
+    noAngkot++;
+}
+for (noAngkot =++ angkotBeroperasi; noAngkot <= jmlAngkot; noAngkot++){
+    console.log('Angkot No. '+ noAngkot + ' Sedang tidak baik2 saja')
+}
