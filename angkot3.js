6f0a5bc347d9aea56064332df347abf2e2ff3e1a
iyas bagus <bagusiyas15@gmail.com>
2024-09-16 22:35:38 +0700

    mempelajari looping dan pengkondisian lagi

---
+++ angkot3.js
@@ -0,0 +1,11 @@
+let jmlAngkot = 10;
+let angkotBeroperasi = 6;
+let noAngkot = 1
+
+for (i = noAngkot; i <= jmlAngkot; i++){
+    if (i <= angkotBeroperasi) {
+        console.log('Angkot No. ' + i + ' Beroprasi dengan baik');
+    } else {
+        console.log('Angkot No. ' + i + ' Sedang tidak baik2 saja');
+    }
+}
